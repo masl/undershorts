a80bd93959bea9a299ffa2d8fcc17a51e2121688
Marcel <contact@masel.dev>
2021-08-18 17:58:33 +0000

    Implement handling yaml

--- cmd/undershorts/main.go
+++ cmd/undershorts/main.go
@@ -19,22 +19,18 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	/*
-			yaml := `
-		- path: /urlshort
-		  url: https://github.com/gophercises/urlshort
-		- path: /urlshort-final
-		  url: https://github.com/gophercises/urlshort/tree/solution
-		`
-			yamlHandler, err := handler.YAMLHandler([]byte(yaml), mapHandler)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("Starting the server on :8080")
-			http.ListenAndServe(":8080", yamlHandler)
-	*/
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := handler.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", mapHandler)
+	http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
--- internal/handler/yaml.go
+++ internal/handler/yaml.go
@@ -1,6 +1,15 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-yaml/yaml"
+)
+
+type Y []struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
@@ -19,6 +28,33 @@ import "net/http"
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+
+	pathMap := buildMap(parsedYaml)
+	if err != nil {
+		return nil, err
+	}
+
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yml []byte) (y Y, err error) {
+	err = yaml.Unmarshal(yml, &y)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func buildMap(yml Y) (pathMap map[string]string) {
+	pathMap = make(map[string]string)
+	for _, v := range yml {
+		pathMap[v.Path] = v.URL
+	}
+
+	return
 }
