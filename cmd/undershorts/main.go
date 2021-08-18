a9f6820145d436fb097c1b383317388d17885344
Marcel <contact@masel.dev>
2021-08-18 17:15:56 +0000

    Implement mapHandler functionality

--- cmd/undershorts/main.go
+++ cmd/undershorts/main.go
@@ -19,18 +19,22 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := handler.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	/*
+			yaml := `
+		- path: /urlshort
+		  url: https://github.com/gophercises/urlshort
+		- path: /urlshort-final
+		  url: https://github.com/gophercises/urlshort/tree/solution
+		`
+			yamlHandler, err := handler.YAMLHandler([]byte(yaml), mapHandler)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Starting the server on :8080")
+			http.ListenAndServe(":8080", yamlHandler)
+	*/
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -11,8 +11,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		longPath, exists := pathsToUrls[r.RequestURI]
+		if exists {
+			http.Redirect(w, r, longPath, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
