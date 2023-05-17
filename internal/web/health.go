5a2046a1412e19f5fcc04b2efbe40b73133afe0f
Marcel <contact@masel.dev>
2023-05-17 17:41:22 +0200

    feat: add database health-check to health endpoint

--- internal/web/health.go
+++ internal/web/health.go
@@ -1,13 +1,21 @@
 package web
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/masl/undershorts/internal/db"
 )
+
+type status map[string]bool
 
 // GET health
 func (w *WebServer) GetHealth(ctx *gin.Context) {
-	// TODO: Add DB health checks
-	ctx.JSON(http.StatusOK, gin.H{"status": "ok"})
+	status := status{
+		"redis":   db.RedisClient.Ping(context.Background()).Err() == nil,
+		"storage": w.store.Ping(),
+	}
+
+	ctx.JSON(http.StatusOK, status)
 }
